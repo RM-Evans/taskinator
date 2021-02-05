454e0e7de640faa9dcac8c0ed3086148c1d364aa
RM-Evans <evansrichardmichael@gmail.com>
2021-02-05 15:53:38 -0700

    now reads user input and creates blocks

--- module-4-assets/js/script.js
+++ module-4-assets/js/script.js
@@ -26,12 +26,29 @@ var createTaskHandler = function() {
     
     //prevent auto refresh
     event.preventDefault();
-
+    //make console display data as JS object  console.dir   .value
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    //console.dir(taskNameInput);
+    //will define and display dropdown text
+    var taskTypeInput = document.querySelector("select[name= 'task-type']").value;
+    //console.log(taskTypeInput);
+    //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new class";
-    tasksToDoEl.appendChild(listItemEl);
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    //give it a class name!
+    taskInfoEl.className = "task-info"
+    // add html content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    //text content from user input
+    // listItemEl.textContent = taskNameInput;
 
-}
+    //ADD ENTIRE LIST ITEM TO LIST
+    tasksToDoEl.appendChild(listItemEl);
+    console.dir(listItemEl);
+
+};
 
 formEl.addEventListener("submit", createTaskHandler);
