6f59cae08151253de6c02be2e6adfaa3f904531b
RM-Evans <evansrichardmichael@gmail.com>
2021-02-04 17:55:13 -0700

    added event listener

--- module-4-assets/js/script.js
+++ module-4-assets/js/script.js
@@ -14,7 +14,7 @@
 // });
 
 
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
@@ -23,10 +23,15 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 //create new list items
 
 var createTaskHandler = function() {
+    
+    //prevent auto refresh
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new class";
     tasksToDoEl.appendChild(listItemEl);
+
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
